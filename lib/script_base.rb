5ab3a1996177491eafc44bbbfba0764367743528
Zach Margolis <zachary.margolis@gsa.gov>
2023-10-20 14:11:01 -0700

    Revert "Base64 decode too"
    This reverts commit a2494ebb6281afb6678caf1e4017d8fd2ee32138.

--- lib/script_base.rb
+++ lib/script_base.rb
@@ -64,13 +64,7 @@ class ScriptBase
 
     if config.deflate?
       require 'zlib'
-      require 'base64'
-      stdout.puts Base64.encode64(
-        Zlib::Deflate.deflate(
-          (result.json || result.table).to_json,
-          Zlib::BEST_COMPRESSION,
-        )
-      )
+      stdout.puts Zlib::Deflate.deflate((result.json || result.table).to_json, Zlib::BEST_COMPRESSION)
     elsif result.json
       stdout.puts result.json.to_json
     else
